b86020f66623ac578963e43d6db19be8171f6c11
Maanya-me <maanyasahums@gmail.com>
2026-04-17 00:56:24 +0530

    Update elevator_controller_tb.v

--- elevator_controller_tb.v
+++ elevator_controller_tb.v
@@ -17,28 +17,34 @@ elevator_controller uut (
     .door_open(door_open)
 );
 
-
 // clock
 always #5 clk = ~clk;
 
 initial begin
 
-clk = 0;
-reset = 1;
-request = 3'b000;
+    clk = 0;
+    reset = 1;
+    request = 3'b000;
 
-#10 reset = 0;
+    // Test Case 1
+    #10 reset = 0;
 
-// request floor 1
-#10 request = 3'b010;
+    // Test Case 2
+    #10 request = 3'b010;
 
-// request floor 2
-#20 request = 3'b100;
+    // Test Case 3
+    #20 request = 3'b100;
 
-// clear
-#20 request = 3'b000;
+    // Test Case 4
+    #20 request = 3'b000;
 
-#50 $stop;
+    // Test Case 5
+    #20 request = 3'b101;
+
+    // Test Case 6
+    #20 request = (1 << floor);
+
+    #50 $stop;
 
 end
 
